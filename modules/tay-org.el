91d3ef903a15843142eacdbdb4578a01749c2965
taylor <taylor@taylors-air.lan>
2021-05-17 06:52:57 -0500

    Update org mode for bullets, clock, and agenda view

--- modules/tay-org.el
+++ modules/tay-org.el
@@ -16,9 +16,35 @@
 (setq org-startup-indented t
       org-agenda-files '("~/orgs")
       org-todo-keywords '((sequence "TODO" "|" "DONE" "ONHOLD"))
-      org-hide-emphasis-markers t
-      org-hide-macro-markers t
-      org-hide-leading-stars t)
+      org-hide-leading-stars t
+      org-fontify-done-headline t
+      org-fontify-quote-and-verse-blocks t
+      org-fontify-whole-heading-line t
+      org-indirect-buffer-display 'current-window
+      org-archive-subtree-save-file-p t)
+
+;; remove the org cycle agenda files binding because it conflicts
+;; with our avy bindings
+(define-key org-mode-map (kbd "C-'") nil)
+
+;; org clock settings for time tracking
+(add-to-list 'org-modules 'org-clock t)
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+
+;; org agenda settings ripped from doom emacs
+(setq-default
+   org-agenda-deadline-faces
+   '((1.001 . error)
+     (1.0 . org-warning)
+     (0.5 . org-upcoming-deadline)
+     (0.0 . org-upcoming-distant-deadline))
+   org-agenda-window-setup 'current-window
+   org-agenda-skip-unavailable-files t
+   org-agenda-span 10
+   org-agenda-start-on-weekday nil
+   org-agenda-start-day "-3d"
+   org-agenda-inhibit-startup t)
 
 (add-to-list 'org-modules 'org-habit t)
 
@@ -34,3 +60,16 @@
          "* TODO %? %i\nDEADLINE:%^t\n")
         ("p" "Personal TODO" entry (file+headline "~/orgs/personal.org" "INBOX")
          "* TODO %? %i\nDEADLINE:%^t\n")))
+
+(tay/do-if-ext-pkg
+ (use-package org-superstar
+   :config
+   (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+   (setq org-superstar-leading-bullet ?\s
+         org-superstar-leading-fallback ?\s
+         org-hide-leading-stars nil
+         org-superstar-todo-bullet-alist
+         '(("TODO" . 9744)
+           ("[ ]"  . 9744)
+           ("DONE" . 9745)
+           ("[X]"  . 9745)))))
