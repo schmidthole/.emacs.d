a33ca1292db4184afcb3ca4f4837ee28f1957c2b
Taylor <taylor.j.schmidt@gmail.com>
2021-06-01 10:56:01 -0500

    Adds cider for clojure and rainbow delimiters

--- modules/tay-clojure.el
+++ modules/tay-clojure.el
@@ -4,3 +4,11 @@
 ;; Packages for clojure dev
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(tay/do-if-ext-pkg
+ (use-package cider
+   :config
+   (setq cider-repl-display-help-banner nil))
+
+ (use-package rainbow-delimiters
+   :config
+   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)))
