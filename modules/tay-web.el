1ce4cdf17ebdff89f028706d1866e530ea9e1ff3
Taylor <taylor.j.schmidt@gmail.com>
2021-08-02 20:58:28 -0500

    adds js2 mode

--- modules/tay-web.el
+++ modules/tay-web.el
@@ -5,6 +5,8 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq js-indent-level 2)
+
 (tay/do-if-ext-pkg
  ;; basically the standard for html, js, and templating for web stuff
  (use-package web-mode
@@ -17,4 +19,15 @@
    (setq web-mode-enable-css-colorization t)
    (setq web-mode-enable-auto-pairing t)
    (setq web-mode-enable-current-element-highlight t)
-   (setq web-mode-enable-current-column-highlight t)))
+   (setq web-mode-enable-current-column-highlight t))
+
+ (use-package js2-mode
+   :config
+   (setq js2-basic-indent 2
+         js2-basic-offset 2
+         js2-auto-indent-p t
+         js2-cleanup-whitespace t
+         js2-enter-indents-newline t
+         js2-indent-on-enter-key t)
+
+   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))))
