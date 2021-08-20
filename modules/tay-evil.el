00977d32c4e0efbdc3bf2420987ad0a2c09815f5
Taylor <taylor.j.schmidt@gmail.com>
2021-08-19 20:30:15 -0500

    disables evil mode

--- modules/tay-evil.el
+++ modules/tay-evil.el
@@ -29,23 +29,25 @@
     :states 'normal
     :keymaps 'override
     :prefix "SPC"
-    "b" 'ivy-switch-buffer
+    "b" 'switch-to-buffer
+    "n" 'switch-to-next-buffer
+    "p" 'switch-to-prev-buffer
     "k" 'tay/kill-this-buffer
-    "x" 'counsel-M-x
+    "x" 'execute-extended-command
     "f" 'find-file
     "g" 'magit-status
     "s" 'swiper
     "a" 'counsel-ag
     "e" 'tay/eshell-new
-    "s" 'tay/vterm-new
+    ;; "s" 'tay/vterm-new
     "c c" 'tay/cleanup
-    "c o a" 'org-agenda
-    "c o c" 'org-capture
-    "1" 'delete-other-windows
-    "0" 'delete-window
-    "2" 'split-window-below
-    "3" 'split-window-right
-    "o" 'other-window
+    ;; "c o a" 'org-agenda
+    ;; "c o c" 'org-capture
+    ;; "1" 'delete-other-windows
+    ;; "0" 'delete-window
+    ;; "2" 'split-window-below
+    ;; "3" 'split-window-right
+    ;; "o" 'other-window
     "w" 'save-buffer))
 
  (use-package evil-goggles
