d06bb0d2bc6ad09926b7ce6746654ed9f0b9951b
Taylor <taylor.j.schmidt@gmail.com>
2021-11-07 08:24:06 -0600

    Adds adoc mode; not very cool

---
+++ modules/tay-adoc.el
@@ -0,0 +1,7 @@
+;; asciidoc editing
+
+(tay/do-if-ext-pkg
+ (use-package adoc-mode
+   :config
+   (add-to-list 'auto-mode-alist (cons "\\.txt\\'" 'adoc-mode))
+   (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))))
