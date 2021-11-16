0920d4e9535c684614cbd179380c287d20c71355
Taylor <taylor.j.schmidt@gmail.com>
2021-11-16 10:22:49 -0600

    adds toggle solarized function

--- modules/tay-theme.el
+++ modules/tay-theme.el
@@ -1,11 +1,27 @@
 (tay/do-if-ext-pkg
- (use-package modus-themes
+ ;; (use-package modus-themes
+ ;;   :config
+ ;;   (setq modus-themes-bold-constructs t)
+ ;;   (setq modus-themes-slanted-constructs t)
+ ;;   (setq modus-themes-prompts '(background intense bold))
+ ;;   (setq modus-themes-fringes nil)
+ ;;   (load-theme 'modus-vivendi t))
+
+ (use-package doom-themes
    :config
-   (setq modus-themes-bold-constructs t)
-   (setq modus-themes-slanted-constructs t)
-   (setq modus-themes-prompts '(background intense bold))
-   (setq modus-themes-fringes nil)
-   (load-theme 'modus-vivendi t))
+   (load-theme 'doom-solarized-dark t)
+   (setq tay/current-theme 'dark)
+   (defun tay/toggle-solarized ()
+     (interactive)
+     (if (string= 'dark tay/current-theme)
+         (progn
+           (disable-theme 'doom-solarized-dark)
+           (load-theme 'doom-solarized-light)
+           (setq tay/current-theme 'light))
+       (progn
+         (disable-theme 'doom-solarized-light)
+         (load-theme 'doom-solarized-dark)
+         (setq tay/current-theme 'dark)))))
 
  (use-package telephone-line
    :config
