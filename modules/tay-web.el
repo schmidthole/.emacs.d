605802a76b1e077a1078ff68f1a8529b90375fa3
Taylor <taylor.j.schmidt@gmail.com>
2021-11-20 10:56:14 -0600

    use lsp mode for tsx and jsx files in web mode

--- modules/tay-web.el
+++ modules/tay-web.el
@@ -11,4 +11,21 @@
    (setq web-mode-enable-css-colorization t)
    (setq web-mode-enable-auto-pairing t)
    (setq web-mode-enable-current-element-highlight t)
-   (setq web-mode-enable-current-column-highlight t)))
+   (setq web-mode-enable-current-column-highlight t)
+
+   :mode (("\\.js\\'" . web-mode)
+	      ("\\.jsx\\'" .  web-mode)
+	      ("\\.ts\\'" . web-mode)
+	      ("\\.tsx\\'" . web-mode)
+	      ("\\.html\\'" . web-mode)))
+
+ (use-package lsp-mode
+   :hook (web-mode . lsp-deferred))
+
+ (use-package rjsx-mode
+   :config
+   (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+   (add-hook 'web-mode-hook
+             (lambda ()
+	           (when (string-equal "jsx" (file-name-extension buffer-file-name))
+                 (rjsx-mode))))))
