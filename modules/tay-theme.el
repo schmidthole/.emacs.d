fbbfb63a5ddab41fa3d1b150ec35874863577efd
Taylor <taylor.j.schmidt@gmail.com>
2022-03-15 23:09:40 -0500

    cleanup dead code in theme

--- modules/tay-theme.el
+++ modules/tay-theme.el
@@ -1,28 +1,4 @@
 (tay/do-if-ext-pkg
- ;; (use-package modus-themes
- ;;   :config
- ;;   (setq modus-themes-bold-constructs t)
- ;;   (setq modus-themes-slanted-constructs t)
- ;;   (setq modus-themes-prompts '(background intense bold))
- ;;   (setq modus-themes-paren-match '(bold intense))
- ;;   (setq modus-themes-headings '((t . (rainbow overline))))
- ;;   (setq modus-themes-fringes nil)
- ;;   (load-theme 'modus-vivendi t))
-
- ;; (use-package doom-themes
- ;;   :config
- ;;   (load-theme 'doom-one t))
-
- ;; (use-package doom-modeline
- ;;   :init
- ;;   (doom-modeline-mode 1)
- ;;   :config
- ;;   (setq doom-modeline-height 8)
- ;;   (setq doom-modeline-bar-width 16)
- ;;   (setq doom-modeline-major-mode-icon nil)
- ;;   (setq doom-modeline-buffer-state-icon nil)
- ;;   (setq doom-modeline-buffer-encoding nil))
-
  (use-package nord-theme
    :config
    (load-theme 'nord t))
