1cffce4f3cf7093ad76199d72177c0c912fea60a
taylor <tschmidt@wavelynxtech.com>
2023-09-15 07:01:06 -0400

    Adds doom modeline back in

--- lisp/tay-modeline.el
+++ lisp/tay-modeline.el
@@ -1,11 +1,19 @@
-(setq-default mode-line-format `("%e"
-                                 mode-line-front-space
-                                 mode-line-buffer-identification
-                                 "   "
-                                 "%l:%c"
-                                 "   "
-                                 mode-name))
-
-
+(use-package doom-modeline
+  :config
+  (setq doom-modeline-icon nil)
+    (doom-modeline-def-modeline 'tay/doom-modeline
+    '(bar
+      matches
+      follow
+      buffer-info
+      buffer-position
+      remote-host
+      selection-info)
+    '(compilation
+      major-mode
+      process))
+  (add-hook 'doom-modeline-mode-hook
+            (lambda () (doom-modeline-set-modeline 'tay/doom-modeline 'default)))
+  (doom-modeline-mode 1))
 
 (provide 'tay-modeline)
